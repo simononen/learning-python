61706a9d8d66cc447780e9448318ade4b3bcb61a
sonen <sonen8524@gmail.com>
2022-04-16 16:45:39 +0300

    Add data types and numbers

---
+++ data_types.py
@@ -0,0 +1,19 @@
+# Fundamental Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# Data Type is a value in python
+
+# Classes -> Custom types
+
+# Specialized Data Types
+# Modules or Extensions
+
+None
+
---
+++ numbers.py
@@ -0,0 +1,8 @@
+# Fundamental Data Types
+# int and float
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))
+
+print(type(0.00024))
