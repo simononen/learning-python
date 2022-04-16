c50a8df16bf54fb85c401ec164275efb7ef0b1b4
sonen <sonen8524@gmail.com>
2022-04-16 23:11:14 +0300

    Add formated_strings and string indexes

---
+++ 05-formated_strings.py
@@ -0,0 +1,6 @@
+name = 'John Doe'
+age = 28
+
+print(f'Hi {name}. You are {age} years old.')
+
+# print('Hi {}. You are {} years old.'.format(name, age))
---
+++ 06-string_indexes.py
@@ -0,0 +1,4 @@
+selfish = 'me me me'
+
+# [start:stop:stepover]
+print(selfish[0:5:3])
